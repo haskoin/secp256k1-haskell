72412cdd3fade51c9d883005e788c03e32e844d9
Enzo Haussecker <enzo@sovereign.io>
2018-03-23 13:54:15 -0700

    Export secret key

--- src/Crypto/Secp256k1/Internal.hs
+++ src/Crypto/Secp256k1/Internal.hs
@@ -480,3 +480,13 @@ foreign import ccall
     -> Ptr RecSig65
     -> Ptr Msg32
     -> IO Ret
+
+foreign import ccall
+    "secp256k1.h secp256k1_ec_privkey_export"
+    ecSecKeyExport
+    :: Ptr Ctx
+    -> Ptr CUChar -- ^ array to store BER-encoded key (allocate 279 bytes)
+    -> Ptr CSize -- ^ size of previous array, will be updated
+    -> Ptr SecKey32
+    -> SerFlags
+    -> IO Ret
